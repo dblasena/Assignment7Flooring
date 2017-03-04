09344829de7ea64f3319d043c8c1021934cdd86d
Dan <dblasena@cs.edu>
2017-03-03 18:43:47 -0600

    Added bonus step

--- app/src/main/java/dblasena/css/assignment7flooring/MainActivity.java
+++ app/src/main/java/dblasena/css/assignment7flooring/MainActivity.java
@@ -6,13 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     private static final int CIS3334_REQUEST_CODE = 1001;
 
     EditText txtWidth, txtLength;
-
-
+    TextView txtTViewResult;
 
 
     @Override
@@ -23,22 +25,36 @@ public class MainActivity extends AppCompatActivity {
         txtWidth = (EditText) findViewById(R.id.txtWidth);
         txtLength = (EditText) findViewById(R.id.txtLength);
 
+        txtTViewResult = (TextView) findViewById(R.id.textViewResult);
 
 
         Button button = (Button) findViewById(R.id.btnResult);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 String width = txtWidth.getText().toString();
-                String length = txtLength.getText().toString() ;
-                Intent secActIntent = new Intent(MainActivity.this, MainActivity2.class);
-                secActIntent.putExtra("Width", width);
-                secActIntent.putExtra("Length", length);
-                startActivity(secActIntent);
+                String length = txtLength.getText().toString();
+                Intent secondActiv = new Intent(MainActivity.this, MainActivity2.class);
+                secondActiv.putExtra("Width", width);
+                secondActiv.putExtra("Length", length);
+                startActivityForResult(secondActiv, CIS3334_REQUEST_CODE);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       if (resultCode == RESULT_OK && requestCode == CIS3334_REQUEST_CODE) {
+            if (data.hasExtra("PrevTotal")) {
+               String result = data.getExtras().getString("PrevTotal");
+                if (result != null && result.length() > 0) {
+                    txtTViewResult.setText("Previous Total = " + result);
+                    txtWidth.setText("");
+                    txtLength.setText("");
+                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+                }
+            }
+       }
 
 
-
+    }
 }
--- app/src/main/java/dblasena/css/assignment7flooring/MainActivity2.java
+++ app/src/main/java/dblasena/css/assignment7flooring/MainActivity2.java
@@ -1,7 +1,10 @@
 package dblasena.css.assignment7flooring;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -35,5 +38,14 @@ public class MainActivity2 extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public void finish() {
+        Intent intent = new Intent();
+        intent.putExtra("PrevTotal", txttotal);
+        setResult(RESULT_OK, intent);
+
+        super.finish();
     }
 }
